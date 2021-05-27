f559a48662ddcd6e3e0ec5824f7868efe18ebd4e
harithilagar <harishthilagar06@gmail.m>
2021-05-27 20:08:17 +0530

    datadriven framework

--- src/test/java/datadrivenframework/constants/UIconstants.java
+++ src/test/java/datadrivenframework/constants/UIconstants.java
@@ -13,5 +13,12 @@ public class UIconstants {
 	public static final String FILE_PATH = "\\src\\test\\resources\\config\\config.properties";
 	
 	public static final String USER_DIR = System.getProperty("user.dir");
+	
+	public static final String USERNAME = "harishthilagar_iWYgUt";
+	
+	public static final String AUTOMATE_KEY = "iRs9pJ83nrMbq7i7vMrp";
+	
+	public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
 
 }
--- src/test/java/datadrivenframework/factory/DriverFactory.java
+++ src/test/java/datadrivenframework/factory/DriverFactory.java
@@ -1,5 +1,7 @@
 package datadrivenframework.factory;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
@@ -7,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import datadrivenframework.constants.UIconstants;
 import datadrivenframework.utils.PropertyReader;
@@ -23,14 +26,14 @@ public class DriverFactory {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 			break;
-			
+
 		case "chromedebug":
 			WebDriverManager.chromedriver().setup();
 			ChromeOptions options = new ChromeOptions();
 			options.setExperimentalOption("debuggerAddress", "localhost:9014");
 			driver = new ChromeDriver(options);
 			break;
-			
+
 		case "headlessChrome":
 			WebDriverManager.chromedriver().setup();
 			ChromeOptions option = new ChromeOptions();
@@ -39,21 +42,40 @@ public class DriverFactory {
 			option.addArguments("headless");
 			driver = new ChromeDriver(option);
 			break;
-			
+
 		case "edge":
 			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
 			break;
-			
+
 		case "firefox":
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 			break;
+
+		case "browserstack":
+			ChromeOptions caps = new ChromeOptions();
+			caps.addArguments("start-maximized");
+			caps.setCapability("os", "Windows");
+			caps.setCapability("os_version", "10");
+			caps.setCapability("browser", "Chrome");
+			caps.setCapability("browser_version", "90");
+			caps.setCapability("name", "harish's Test");
+			try {
+				driver = new RemoteWebDriver(new URL(UIconstants.URL), caps);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+			break;
+			
+		default:
+			driver = null;
+			break;
 		}
 		int wait = Integer.parseInt(prop.getProperty(UIconstants.IMPLICIT_WAIT));
 		driver.get(prop.getProperty(UIconstants.BASE_URL));
-        driver.manage().timeouts().implicitlyWait(wait, TimeUnit.SECONDS);
-        driver.manage().window().maximize();    
+		driver.manage().timeouts().implicitlyWait(wait, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 		return driver;
 	}
 
